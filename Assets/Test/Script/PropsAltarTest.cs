b8a342d9d983b4d7cfa2124ff325accfcbab1801
ngandolh <nganttk002@gmail.com>
2024-02-27 10:54:35 +0700

    Fix Ruin - Open Windown

--- Assets/Test/Script/PropsAltarTest.cs
+++ Assets/Test/Script/PropsAltarTest.cs
@@ -8,9 +8,9 @@ namespace Test
     public class PropsAltarTest : MonoBehaviour
     {
         public List<SpriteRenderer> runes;
-        public float lerpSpeed;
-        private Color curColor;
-        private Color targetColor;
+        public float lerpSpeed = 3;
+        private Color curColor = new Color(1, 1, 1, 1);
+        private Color targetColor = new Color(1, 1, 1, 0);
         private bool activated = false;
 
         private void OnTriggerEnter2D(Collider2D other)
